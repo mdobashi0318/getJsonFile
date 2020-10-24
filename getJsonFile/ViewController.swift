389e4d49cca571078a0e629a1674415df4a3a78a
Masaharu Dobashi <dobashi.masaharu@gmail.com>
2020-10-24 16:50:06 +0900

    Initial Commit

---
+++ getJsonFile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  getJsonFile
+//
+//  Created by 土橋正晴 on 2020/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
